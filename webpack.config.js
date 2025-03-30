1b41cad747d9ab7c33e86b8737a6dcfc2360b7fb
undiaxinus <undiaxinus@gmail.com>
2025-03-30 19:27:11 +0800

    Update app configuration and enhance build scripts
    - Added new properties to app.json for improved web app configuration, including name, shortName, lang, themeColor, and backgroundColor.
    - Updated favicon path and startUrl in app.json for better web app experience.
    - Modified package.json to include new build scripts for web and general builds, and updated react-dom dependency version.
    - Enhanced webpack.config.js by removing unnecessary custom configurations, streamlining the setup for Expo.

--- webpack.config.js
+++ webpack.config.js
@@ -2,31 +2,6 @@ const path = require('path');
 const createExpoWebpackConfigAsync = require('@expo/webpack-config');
 
 module.exports = async function (env, argv) {
-  const config = await createExpoWebpackConfigAsync({
-    ...env,
-    babel: {
-      dangerouslyAddModulePathsToTranspile: [
-        '@react-native-masked-view/masked-view',
-        'react-native-gesture-handler',
-      ],
-    },
-  }, argv);
-
-  config.devServer = {
-    ...config.devServer,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
-      'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
-    },
-  };
-
-  // Add resolve.alias if it doesn't exist
-  config.resolve = config.resolve || {};
-  config.resolve.alias = config.resolve.alias || {};
-  
-  // Add this alias
-  config.resolve.alias['react-native-gesture-handler'] = 'react-native-gesture-handler/lib/commonjs/web';
-
+  const config = await createExpoWebpackConfigAsync(env, argv);
   return config;
 }; 
